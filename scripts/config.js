8e35275b7ebce11f898243751d318ca92b992c28
Wen Yuan <wenyuanchin229@gmail.com>
2021-12-13 22:20:18 +0800

    extract config

---
+++ scripts/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAXk8gPSjviouGc-pfkoXdFzHDcy_fR-dM",
+    authDomain: "adminproject-129f1.firebaseapp.com",
+    databaseURL: "https://adminproject-129f1.firebaseio.com",
+    projectId: "adminproject-129f1",
+    storageBucket: "adminproject-129f1.appspot.com",
+    messagingSenderId: "52547207023731",
+    appId: "1:944478240098:android:794f67ffbebd2508bac03b",
+};
